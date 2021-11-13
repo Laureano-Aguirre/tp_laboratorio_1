5f14b33b38952b54ec38500c47e69812157d9749
Laureano-Aguirre <78192480+Laureano-Aguirre@users.noreply.github.com>
2021-11-13 03:05:45 -0300

    Add files via upload

---
+++ TP3-LABO 2021/tp3_windows/Aguirre.h
@@ -0,0 +1,15 @@
+/*
+ * Aguirre.h
+ *
+ *  Created on: 6 oct. 2021
+ *      Author: Lau
+ */
+
+#ifndef AGUIRRE_H_
+#define AGUIRRE_H_
+
+void getInt(char* mensaje, int* entero);
+void getFloat(char* mensaje, float* flotante);
+void getString(char* mensaje, char* cadena);
+
+#endif /* AGUIRRE_H_ */
