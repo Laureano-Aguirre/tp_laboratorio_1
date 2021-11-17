76a80821016bd4b97ca2f02a91d56cee2f654712
Laureano-Aguirre <laureanoaguirre2911@gmail.com>
2021-11-17 15:16:28 -0300

    Revert "actualizado tp3" ultimaversion
    This reverts commit 79d7db5389227185df12c5c8c05b4077755cee94.

--- TP 3/tp3_windows/main.c
+++ TP 3/tp3_windows/main.c
@@ -15,7 +15,7 @@
      4. Modificar datos de empleado
      5. Baja de empleado
      6. Listar empleados
-     7. Ordenar empleados-
+     7. Ordenar empleados
      8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
      9. Guardar los datos de los empleados en el archivo data.csv (modo binario).
     10. Salir
@@ -27,12 +27,7 @@ int main()
 {
 	setbuf(stdout, NULL);
     int option = 0;
-    int retornoCargaTexto=-1;
-    int retornoCargaBinario=-1;
-    int idEmp;
-    int resp;
 
-    Employee* empleado;
     LinkedList* listaEmpleados = ll_newLinkedList();
     do{
 
@@ -52,141 +47,22 @@ int main()
         switch(option)
         {
             case 1:
-            	puts("\nUsted ha elegido la opcion cargar los datos de los empleados desde el archivo data.csv (en modo texto).");
-                if(retornoCargaTexto==-1)
-                {
-                	if(retornoCargaBinario==-1)
-                	{
-                		if(controller_loadFromText("data.csv",listaEmpleados)==1)
-                		{
-                			retornoCargaTexto=1;
-                			empleado=(Employee*)ll_get(listaEmpleados, ll_len(listaEmpleados)-1);
-                			employee_getId(empleado, &idEmp);
-                		}
-                	}
-                	else
-                	{
-                		getInt("Ya hay datos cargados. Desea sobreescribir los datos del sistema? 1.si 2.no: ", &resp);
-                		while(resp!=1 && resp!=2)
-                		{
-                			getInt("Error, ya hay datos cargados. Desea sobreescribir los datos del sistema? 1.si 2.no: ", &resp);
-                		}
-                		if(resp==1)
-                		{
-                			ll_clear(listaEmpleados);
-                			if(controller_loadFromText("data.csv",listaEmpleados)==1)
-                			{
-                				retornoCargaTexto=1;
-                				empleado=(Employee*)ll_get(listaEmpleados, ll_len(listaEmpleados)-1);
-                				employee_getId(empleado, &idEmp);
-                			}
-                		}
-                		else
-                		{
-                			puts("\nOperacion cancelada.");
-                		}
-                	}
-                }
-                else
-                {
-                	getInt("Ya hay datos cargados. Desea sobreescribir los datos del sistema? 1.si 2.no: ", &resp);
-                	while(resp!=1 && resp!=2)
-                	{
-                		getInt("Error, ya hay datos cargados. Desea sobreescribir los datos del sistema? 1.si 2.no: ", &resp);
-                	}
-                	if(resp==1)
-                	{
-                		ll_clear(listaEmpleados);
-                		if(controller_loadFromText("data.csv",listaEmpleados)==1)
-                		{
-                			retornoCargaTexto=1;
-                			empleado=(Employee*)ll_get(listaEmpleados, ll_len(listaEmpleados)-1);
-                			employee_getId(empleado, &idEmp);
-                		}
-                	}
-                	else
-                	{
-                		puts("\nOperacion cancelada.");
-                	}
-                }
+                controller_loadFromText("data.csv",listaEmpleados);
                 break;
             case 2:
             	puts("\nUsted ha elegido la opcion cargar los datos de los empleados desde el archivo data.csv (en modo binario).");
-            	if(retornoCargaBinario==-1)
-            	{
-            		if(retornoCargaTexto==-1)
-            		{
-            			if(controller_loadFromBinary("data.bin", listaEmpleados)==1)
-            			{
-            				retornoCargaBinario=1;
-            				empleado=(Employee*)ll_get(listaEmpleados, ll_len(listaEmpleados)-1);
-            				employee_getId(empleado, &idEmp);
-            			}
-            		}
-            		else
-            		{
-            			getInt("Ya hay datos cargados. Desea sobreescribir los datos del sistema? 1.si 2.no: ", &resp);
-            			while(resp!=1 && resp!=2)
-            			{
-            				getInt("Error, ya hay datos cargados. Desea sobreescribir los datos del sistema? 1.si 2.no: ", &resp);
-            			}
-            			if(resp==1)
-            			{
-            				ll_clear(listaEmpleados);
-            				if(controller_loadFromBinary("data.bin", listaEmpleados)==1)
-            				{
-            					retornoCargaBinario=1;
-            					empleado=(Employee*)ll_get(listaEmpleados, ll_len(listaEmpleados)-1);
-            					employee_getId(empleado, &idEmp);
-            				}
-            			}
-            			else
-            			{
-            				puts("\nOperacion cancelada");
-            			}
-            		}
-            	}
-            	else
-            	{
-            		getInt("Ya hay datos cargados. Desea sobreescribir los datos del sistema? 1.si 2.no: ", &resp);
-            		while(resp!=1 && resp!=2)
-            		{
-            			getInt("Error, ya hay datos cargados. Desea sobreescribir los datos del sistema? 1.si 2.no: ", &resp);
-            		}
-            		if(resp==1)
-            		{
-            			ll_clear(listaEmpleados);
-            			if(controller_loadFromBinary("data.bin", listaEmpleados)==1)
-            			{
-            				retornoCargaBinario=1;
-            				empleado=(Employee*)ll_get(listaEmpleados, ll_len(listaEmpleados)-1);
-            				employee_getId(empleado, &idEmp);
-            			}
-            		}
-            		else
-            		{
-            			puts("\nOperacion cancelada");
-            		}
-            	}
+            	controller_loadFromBinary("data.bin", listaEmpleados);
             	break;
             case 3:
             	puts("\nUsted ha seleccionado la opcion Alta empleado.");
-            	if(retornoCargaTexto==1 || retornoCargaBinario==1)
+            	if(controller_addEmployee(listaEmpleados)==1)
             	{
-            		if(controller_addEmployee(listaEmpleados, &idEmp)==1)
-            		{
-            			puts("\nEmpleado cargado correctamente.");
-            		}
-            		else
-            		{
-            			puts("\nNo se pudo cargar el empleado.");
-            		}
+            		puts("\nEmpleado cargado correctamente.");
             	}
             	else
             	{
-            		puts("\nError, debe cargar los datos del archivo antes de dar de alta a un empleado.");
+            		puts("\nNo se pudo cargar el empleado.");
             	}
-
             	break;
             case 4:
             	puts("\nUsted ha elegido la opcion modificar datos de empleado.");
